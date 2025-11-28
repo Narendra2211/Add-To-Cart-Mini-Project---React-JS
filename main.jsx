5340f16e12c15e367ddb4cae1f275f5cd935b55b
Narendra Surthani <narendras5214@gmail.com>
2025-11-28 22:25:59 +0530

    Create Main component with Addtocart integration
    The Main component serves as the central or root-level functional component that loads the primary features of the application. Its main responsibility is to act as a container or layout section that embeds other major components. In this case, it renders the Addtocart component as the main functional unit of the application.

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import '../css/main.css'
+import Addtocart from "./Addtocartindexbased";
+function Main(){
+  return(
+        <main>
+            <Addtocart/>
+        </main>
+    )
+}
+export default Main;
